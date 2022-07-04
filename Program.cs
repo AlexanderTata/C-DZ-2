23d06b363cef57d6fcd34496b2aa238a793c1f3c
Alexander Tata <ta1996@yandex.ru>
2022-07-04 16:46:43 +0300

    Решение первой задачи

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿{ /*Задача №1: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+if (number.Length > 2) Console.WriteLine(number[2]);
+else Console.WriteLine("Длина массива менее трёх элементов");
+
+/*int number = 123456789; // математический способ решения задачи
+int digital = 0;
+while (number/100 > 0) 
+{
+    digital = number % 10;
+    number/=10;
+}
+Console.WriteLine(digital);*/   
+}
+
+{ /*Задача №2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+
+
+
+
+}
+
+
+
